123bf4992a6b04a943e9da0d1bd6b8501a7332eb
kalebhenrique <kalebhenriquebr@gmail.com>
2022-10-07 20:32:41 -0300

    category tests failed 3 examples, 2 failures

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Testing factory" do
+    it { expect(build(:category)).to be_valid }
+  end
+
+  context "Validating category name nil" do
+    it " should be invalid" do
+      expect(build(:category, name: nil)).to be_invalid
+    end
+
+    it " should be invalid if repeated" do
+      create(:category, name: "Celulares")
+      expect(build(:category, name: "Celulares")).to be_invalid
+    end
+  end
 end
