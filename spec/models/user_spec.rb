b05f6dffeff9adb3fd6180ef315b2d04f909fc39
kalebhenrique <kalebhenriquebr@gmail.com>
2022-10-13 20:58:35 -0300

    user tests failure

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Testing factory" do
+    it { expect(build(:user)).to be_valid }
+    it { expect(build(:user, :admin)).to be_valid }
+  end
+
+  context "Validating wallet" do
+    it " should be invalid if nil" do
+      expect(build(:user, wallet: nil)).to be_invalid
+    end
+    it " should be invalid if double" do
+      expect(build(:user, wallet: 1.1)).to be_invalid
+    end
+  end
+
+  context "Validating is_admin" do
+    it " should be invalid if nil" do
+      expect(build(:user, is_admin: nil)).to be_invalid
+    end
+  end
 end
