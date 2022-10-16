7c52fc266f1d2b117aea8c30da5a508d48f6be5c
kalebhenrique <kalebhenriquebr@gmail.com>
2022-10-16 15:40:37 -0300

    brands, categories, products factories updated

--- spec/factories/brands.rb
+++ spec/factories/brands.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
+  # Nome de brand deve ser único. Devemos garantir que chamae
+  # create(:brand) duas vezes não irá dar problemas por usar o
+  # mesmo nome, então criamos uma sequencia para que o nome nunca
+  # se repita, a nao ser que propositalmente
+  sequence :brand_names do |n|
+    "Brand #{n}"
+  end
   factory :brand do
-    name { "MyString" }
+    # generate chama a sequência.
+    name { generate(:brand_names) }
   end
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
+  sequence :product_names do |n|
+    "Product #{n}"
+  end
+  sequence :product_description do |n|
+    "Product #{n}"
+  end
+  
   factory :product do
-    name { "MyString" }
+    name { generate(:product_names) }
     price { 1 }
-    description { "MyString" }
+    description { generate(:product_description) }
     inventory { 1 }
-    brand { nil }
-    category { nil }
+    brand { association :brand }
+    category { association :category }
   end
 end
