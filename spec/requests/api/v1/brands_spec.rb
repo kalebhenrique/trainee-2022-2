82c23a7a3e09f004f590e502727dab7fe0bac6e0
kalebhenrique <kalebhenriquebr@gmail.com>
2022-10-22 17:27:44 -0300

    brands tests #1 passing

--- spec/requests/api/v1/brands_spec.rb
+++ spec/requests/api/v1/brands_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Brands", type: :request do
   describe "GET /index" do
     before do
-      create(:brands, id:1, name:"Apple")
-      create(:brands, id:2, name:"Samsung")
-      create(:brands, id:3, name:"Logitech")
+      create(:brand, id:1, name:"Apple")
+      create(:brand, id:2, name:"Samsung")
+      create(:brand, id:3, name:"Logitech")
     end
     context 'when index return' do
       before do
@@ -15,20 +15,26 @@ RSpec.describe "Api::V1::Brands", type: :request do
         expect(response).to have_http_status(:ok)
       end
       it 'return a json' do
-        expect(response.content_type).to eq('application/json;charset=utf-8')
+        expect(response.content_type).to eq('application/json; charset=utf-8')
       end
       it 'return created instances' do
         expect(JSON.parse(response.body)).to eq([{
           'id' => 1,
-          'name' => "Apple"
+          'name' => "Apple",
+          'created_at' => eval(Brand.find(1).created_at.to_json),
+          'updated_at' => eval(Brand.find(1).updated_at.to_json)
         },
         {
           'id' => 2,
-          'name' => "Samsung"
+          'name' => "Samsung",
+          'created_at' => eval(Brand.find(2).created_at.to_json),
+          'updated_at' => eval(Brand.find(2).updated_at.to_json)
         },
         {
           'id' => 3,
-          'name' => "Logitech"
+          'name' => "Logitech",
+          'created_at' => eval(Brand.find(3).created_at.to_json),
+          'updated_at' => eval(Brand.find(3).updated_at.to_json)
         }
         ])
       end
