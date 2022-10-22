9156a2739618f09f6f915bb50ce94d81ca70b165
kalebhenrique <kalebhenriquebr@gmail.com>
2022-10-22 17:16:30 -0300

    brands tests #1

--- spec/requests/api/v1/brands_spec.rb
+++ spec/requests/api/v1/brands_spec.rb
@@ -2,10 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Brands", type: :request do
   describe "GET /index" do
-    it 'return http status ok' do
-      get '/api/v1/brands/index'
-      expect(response). to have_http_status(:ok)
+    before do
+      create(:brands, id:1, name:"Apple")
+      create(:brands, id:2, name:"Samsung")
+      create(:brands, id:3, name:"Logitech")
     end
-    
+    context 'when index return' do
+      before do
+        get '/api/v1/brands/index'
+      end
+      it 'return http status ok' do
+        expect(response).to have_http_status(:ok)
+      end
+      it 'return a json' do
+        expect(response.content_type).to eq('application/json;charset=utf-8')
+      end
+      it 'return created instances' do
+        expect(JSON.parse(response.body)).to eq([{
+          'id' => 1,
+          'name' => "Apple"
+        },
+        {
+          'id' => 2,
+          'name' => "Samsung"
+        },
+        {
+          'id' => 3,
+          'name' => "Logitech"
+        }
+        ])
+      end
+    end
   end
 end
