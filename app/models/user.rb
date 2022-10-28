2a789a2d37d75c16ef145a662e3b568eeb8379a8
Bernardo <bernardobraga2016@gmail.com>
2022-10-28 10:42:26 -0300

    devise installed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :carts
     validates :wallet, presence: :true
     validates :is_admin, inclusion: [true, false], exclusion: [nil]
